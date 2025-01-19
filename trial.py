77457a439404cf64f83cae1d6fe7e35f8d41a251
AbhiramHande <105887433+AbhiramHande@users.noreply.github.com>
2025-01-19 23:27:12 +0530

    Update trial.py

--- trial.py
+++ trial.py
@@ -4,11 +4,13 @@ from time import sleep
 
 def main():
     led = LED(17)
-    
+
     for _ in range(16):
         led.on()
+        print(f"GPIO17 is {'HIGH' if led.is_active else 'LOW'}")
         sleep(1)
         led.off()
+        print(f"GPIO17 is {'HIGH' if led.is_active else 'LOW'}")
         sleep(1)
 
 main()
