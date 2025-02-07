996c6c29dc15b319f766662b972e8f6afe95fb7c
Abhiram H <105887433+AbhiramHande@users.noreply.github.com>
2025-02-07 18:46:43 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-sudo python3 rpi_trial.py
+if [ $(uname -r | grep -c 'rpi') -ge 1 ]; then
+    sudo python3 rpi_trial.py
+elif [ $(uname -r | grep -c 'tegra') -ge 1 ]; then 
+    sudo python3 jetson_trial.py
+fi
