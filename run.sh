504092aeb50908f1e9f34d50f65cbb0d5eee7c10
AbhiramHande <abhiram.h2003@gmail.com>
2025-02-18 06:38:31 +0530

    Modify run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-if [ $(uname -r | grep -c 'rpi') -ge 1 ]; then
-    sudo python3 rpi_trial.py
-elif [ $(uname -r | grep -c 'tegra') -ge 1 ]; then 
-    sudo python3 jetson_trial.py
+if [ "$(uname -r | grep -c 'rpi')" -ge 1 ]; then
+    sudo python3 Raspberry/ultrasound.py
+elif [ "$(uname -r | grep -c 'tegra')" -ge 1 ]; then 
+    sudo python3 Jetson/motor.py
+    sudo python3 Jetson/ultrasound.py
 fi
