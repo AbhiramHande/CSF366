16012768679ace8cc624818be530ab50c26501a1
AbhiramHande <abhiram.h2003@gmail.com>
2025-03-17 13:52:32 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ if [ "$(uname -r | grep -c 'rpi')" -ge 1 ]; then
     sudo python3 Raspberry/ultrasound.py
 elif [ "$(uname -r | grep -c 'tegra')" -ge 1 ]; then 
     sudo python3 Jetson/motor.py
-    #sudo python3 Jetson/ultrasound.py
+    sudo python3 Jetson/ultrasound.py
 fi
