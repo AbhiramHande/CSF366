e6fa35f79e698702c116bb5eb4280680352cf69d
AbhiramHande <abhiram.h2003@gmail.com>
2025-05-16 12:53:43 +0530

    Add side ultrasound sensor

--- Jetson/ultrasound.py
+++ Jetson/ultrasound.py
@@ -3,29 +3,53 @@ import Jetson.GPIO as GPIO
 from time import sleep, time
 
 # Define GPIO Pins
-TRIG = 21           #Physical pin 40
-ECHO = 20           #Physical pin 38
+front_trig = 21           # Physical pin 40
+front_echo = 20           # Physical pin 38
+
+side_trig = 6             # Physical pin 31
+side_echo = 5             # Physical pin 29
 
 # Setup GPIO Mode
 def setup_ultrasound():
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(TRIG, GPIO.OUT)
-    GPIO.setup(ECHO, GPIO.IN)
+    GPIO.setup(front_trig, GPIO.OUT)
+    GPIO.setup(front_echo, GPIO.IN)
+    GPIO.setup(side_trig, GPIO.OUT)
+    GPIO.setup(side_echo, GPIO.IN)
 
-def measure_distance():
-    GPIO.output(TRIG, False)
+# Function to measure front sensor distance
+def measure_front_distance():
+    GPIO.output(front_trig, False)
     sleep(0.1)
-    GPIO.output(TRIG, True)
+    GPIO.output(front_trig, True)
     sleep(0.00001)
-    GPIO.output(TRIG, False)
+    GPIO.output(front_trig, False)
 
-    while GPIO.input(ECHO) == 0:
+    while GPIO.input(front_echo) == 0:
         pulse_start = time()
 
     # Wait for the echo signal to go LOW
-    while GPIO.input(ECHO) == 1:
+    while GPIO.input(front_echo) == 1:
         pulse_end = time()
 
+    pulse_duration = pulse_end - pulse_start
+    distance = (pulse_duration * 34300) / 2  # in cm
+    return round(distance, 2)
+
+# Function to measure side sensor distance
+def measure_side_distance():
+    GPIO.output(side_trig, False)
+    sleep(0.1)
+    GPIO.output(side_trig, True)
+    sleep(0.00001)
+    GPIO.output(side_trig, False)
+
+    while GPIO.input(side_echo) == 0:
+        pulse_start = time()
+
+    # Wait for the echo signal to go LOW
+    while GPIO.input(side_echo) == 1:
+        pulse_end = time()
 
     pulse_duration = pulse_end - pulse_start
     distance = (pulse_duration * 34300) / 2  # in cm
@@ -40,8 +64,11 @@ if __name__ == "__main__":
             print(f"Waiting for input... ({elapsed_time}s elapsed)", end='\r')
             if keyboard.is_pressed('d'):
                 elapsed_time = 0
-                dist = measure_distance()
-                print(f"\nDistance: {dist} cm")
+                # Run distance measurements in parallel using threads
+                dist = measure_front_distance()
+                print(f"\nFront Distance: {dist} cm")
+                dist = measure_side_distance()
+                print(f"\nSide Distance: {dist} cm")
                 sleep(0.2)
             elif keyboard.is_pressed('q'):
                 print("\nExiting...")
